6ca4aed9ab3d22ebb1b80358d779f1cce69abf2a
Seth Watson <seth.watson1@gmail.com>
2017-10-03 17:27:21 -0700

    scroll animation

--- js/main-page.js
+++ js/main-page.js
@@ -1,10 +1,22 @@
-$(document).ready(function (){
-let onepage = $(".onepage");
-let root = $("html, body");
+$(document).ready(function () {
+    let onepage = $(".onepage");
+    let root = $("html, body");
 
-for(i = 0; i < onepage.length; i++) {
-    let id = onepage[i].id;
+    for (i = 0; i < onepage.length; i++) {
+        let id = onepage[i].id;
 
-    $("#"+id).append("<a href='"+id+"'></a>")
-}
+        $("#" + id).append("<a href='" + id + "'> </a>")
+    }
+
+    $("a").click(function () {
+
+        if ($.attr(this, "href").indexOf("#") != -1 && $.attr(this, "href") != "#") {
+
+            console.log($.attr(this, "href"));
+            root.animate({
+                scrollTop: $($.attr(this, "href")).offset().top
+            }, 1000);
+        }
+
+    });
 });
